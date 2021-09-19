7d5ba4f83672d9cdead096e319572c713993196d
Mahamud Arteh <artehrush@gmail.com>
2021-09-20 00:39:33 +0100

    image placement changed & styling applied to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,13 @@ const About = () => {
 				<div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
 					<h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
 						Hi, I'm Mahamud.
-						<br className='hidden lg:inline-block' />I enjoy building
-						applications
+						<br />
+						<img src={profilePic} />
 					</h1>
+					<h2>
+						<p className='hidden lg:inline-block' />I enjoy building
+						applications
+					</h2>
 					<p className='mb-8 leading-relaxed'>
 						Hello, my name is Mahamud Arteh, and I am a recently graduated
 						full-stack engineer. I can handle all the work of databases,
@@ -61,9 +65,7 @@ const About = () => {
 						</a>
 					</div>
 				</div>
-				<div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
-					<img src={profilePic} />
-				</div>
+				<div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'></div>
 			</div>
 		</section>
 	);
