57e24bed6020787bdb92e9d233c091e0e77d048d
Mahamud Arteh <artehrush@gmail.com>
2021-09-20 14:32:46 +0100

    react logo introduced

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import About from './components/About';
-import Contact from './components/Contact';
-import NavBar from './components/NavBar';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import "./App.css";
+import logo from "./logo.svg";
 // import Testimonials from ',.components/Testimonials';
 
-import './App.css';
+import "./App.css";
 
 function App() {
 	return (
-		<main className='text-gray-400 bg-gray-900 body-font'>
+		<main className="text-gray-400 bg-gray-900 body-font">
+			<img src={logo} className="App-logo" />
 			{/* <NavBar /> */}
 			<About />
-			<Projects />
+			{/* <Projects /> */}
 			{/* <Skills /> */}
 			{/* <Testimonials /> */}
 		</main>
